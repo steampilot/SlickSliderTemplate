61e60bfe281bdc1c8452f5f6d2cbb73c2436721d
Steampilot <github@steampilot.ch>
2016-08-31 10:12:42 +0200

    Added Lorempixel placeholder images to slider.php

--- src/slider.php
+++ src/slider.php
@@ -14,15 +14,19 @@
     <div class="slider">
         <div class="col-xs-12 col-md-6 col-lg-3 panel panel-heading panel-danger">
             <h1 class="bg-danger">Rot</h1>
+            <img src="http://lorempixel.com/1200/800/sports" class="img-responsive" alt="Lorempixel">
         </div>
         <div class="col-xs-12 col-md-6 col-lg-3 panel panel-heading panel-success">
             <h1 class="bg-success">Grün</h1>
+            <img src="http://lorempixel.com/1200/800/animals" class="img-responsive" alt="Lorempixel">
         </div>
         <div class="col-xs-12 col-md-6 col-lg-3 panel panel-heading panel-info">
             <h1 class="bg-info">Blau</h1>
+            <img src="http://lorempixel.com/1200/800/business" class="img-responsive" alt="Lorempixel">
         </div>
         <div class="col-xs-12 col-md-6 col-lg-3 panel panel-heading panel-warning">
             <h1 class="bg-warning">Gelb</h1>
+            <img src="http://lorempixel.com/1200/800/food" class="img-responsive" alt="Lorempixel">
         </div>
     </div>
 </section>
